59880f3741b5dba17ffc2e4c66d876550055801f
RafiBomb <rafi@zurb.com>
2017-03-29 16:16:34 -0700

    adds toggle js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -22,3 +22,29 @@ $('#cssToggle').click(function(){
     toggleCode();
   }
 });
+
+
+// toggles trigger for the code boxes
+$('[data-toggle-HTML]').click(function() {
+  $(this).toggleClass('is-active');
+  $('#codeBoxHTML').toggleClass('is-active');
+  // if($!('[data-toggle-JS], [data-toggle-SCSS]').hasClass('is-active')) {
+
+  // }
+});
+
+$('[data-toggle-SCSS]').click(function() {
+  $(this).toggleClass('is-active');
+  $('#codeBoxSCSS').toggleClass('is-active');
+});
+
+// if($('[data-toggle-JS]').hasClass('is-active')) {
+//   $('[data-toggle-JS]').click(function() {
+//     $(this).toggleClass('is-active');
+//     $('#codeBoxJS').toggleClass('is-active');
+//   });
+// }
+// else {
+//   do nothing
+// }
+
