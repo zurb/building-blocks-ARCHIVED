8c319d5fd71e29af78566269a55be7135c65d7de
canterberrie <christine@zurb.com>
2017-04-04 10:14:02 -0700

    fix card select js

--- src/building-blocks/card-select/card-select.js
+++ src/building-blocks/card-select/card-select.js
@@ -1,5 +1,5 @@
-$(document).foundation();
-
+$('[data-cardSelectButton]').click(function() {
+  $(this).parent('[data-cardSelect]').toggleClass("is-selected");
 $('.button').bind('click', function(event) {
   $(".card").toggleClass("is-selected");
 });
