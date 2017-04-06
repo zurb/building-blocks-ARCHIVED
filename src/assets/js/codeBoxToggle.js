80c1ed1131bca698e008bc34491e142f8cb8e72e
Tim Hartwick <tdhartwick2@gmail.com>
2017-04-06 15:09:53 -0700

    set up area for small only logic

--- src/assets/js/codeBoxToggle.js
+++ src/assets/js/codeBoxToggle.js
@@ -2,27 +2,33 @@ var toggleHTML = $('[data-toggle-HTML]');
 var toggleStyles = $('[data-toggle-styles]');
 var toggleJS   = $('[data-toggle-JS]');
 
-// toggles trigger for the code boxes
-toggleHTML.click(function(e) {
-  if( toggleStyles.hasClass('is-active') || toggleJS.hasClass('is-active') ) {
-    $(this).toggleClass('is-active');
-    $('#codeBoxHTML').toggleClass('is-active');
+$(window).on("load resize", function() {
+  //On small only show one at a time
+  if (Foundation.MediaQuery.current == 'small') {
+    console.log('small');
   }
-  e.preventDefault();
-});
+  // toggles trigger for the code boxes
+  toggleHTML.click(function(e) {
+    if( toggleStyles.hasClass('is-active') || toggleJS.hasClass('is-active') ) {
+      $(this).toggleClass('is-active');
+      $('#codeBoxHTML').toggleClass('is-active');
+    }
+    e.preventDefault();
+  });
 
-toggleStyles.click(function(e) {
-  if( toggleHTML.hasClass('is-active') || toggleJS.hasClass('is-active') ) {
-    $(this).toggleClass('is-active');
-    $('#codeBoxStyles').toggleClass('is-active');
-  }
-  e.preventDefault();
-});
+  toggleStyles.click(function(e) {
+    if( toggleHTML.hasClass('is-active') || toggleJS.hasClass('is-active') ) {
+      $(this).toggleClass('is-active');
+      $('#codeBoxStyles').toggleClass('is-active');
+    }
+    e.preventDefault();
+  });
 
-toggleJS.click(function(e) {
-  if( toggleHTML.hasClass('is-active') || toggleStyles.hasClass('is-active') ) {
-    $(this).toggleClass('is-active');
-    $('#codeBoxJS').toggleClass('is-active');
-  }
-  e.preventDefault();
+  toggleJS.click(function(e) {
+    if( toggleHTML.hasClass('is-active') || toggleStyles.hasClass('is-active') ) {
+      $(this).toggleClass('is-active');
+      $('#codeBoxJS').toggleClass('is-active');
+    }
+    e.preventDefault();
+  });
 });
