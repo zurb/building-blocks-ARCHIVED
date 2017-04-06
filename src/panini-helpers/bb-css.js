8c912cb9846572974459d1be401522fd788929be
John Cheng <john.k.f.cheng@gmail.com>
2017-04-06 15:53:19 -0700

    Handle empty in panini helpers to unbreak static build

--- src/panini-helpers/bb-css.js
+++ src/panini-helpers/bb-css.js
@@ -2,6 +2,10 @@ var fs = require('fs')
 module.exports = function(page) {
   if(page) {
     var baseName = page.split('.')[0]
-    return fs.readFileSync('dist/building-block/' + baseName + "/" + baseName + '.css', 'utf-8');
+    try {
+      return fs.readFileSync('dist/building-block/' + baseName + "/" + baseName + '.css', 'utf-8');
+    } catch(ex) {
+      return '';
+    }
   }
 }
